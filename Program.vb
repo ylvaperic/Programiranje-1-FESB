0f58a2c9d0022f48719e565a1438778449004d27
Ilva Perić <iperic03@fesb.hr>
2024-10-14 23:56:54 +0200

    Add files via upload

---
+++ Program.vb
@@ -0,0 +1,23 @@
+﻿'Napišite program koji će od korisnika tražiti unos veličine stranica trokuta. Provjerom
+'unesenih stranica potrebno je ispisati o kojoj vrsti trokuta se radi.
+'Ilva Perić, 1. godina prijediplomskog stručnog studija računarstva 2024/2025
+
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim a, b, c As Integer
+        Console.WriteLine("Unesite 3 stranice trokuta")
+        a = Console.ReadLine()
+        b = Console.ReadLine()
+        c = Console.ReadLine()
+        If (a = b And a = c) Then
+            Console.WriteLine("Trokut je jednostraničan")
+        ElseIf a = b Then
+            Console.WriteLine("Trokut je jednakokračan")
+        Else
+            Console.WriteLine("Trokut je raznostraničan")
+        End If
+
+    End Sub
+End Module
